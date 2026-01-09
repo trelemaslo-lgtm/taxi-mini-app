15675c940d09ea3cf4555d30b903f56b3685e21d
trelemaslo-lgtm <trelemaslo@gmail.com>
2026-01-09 10:31:51 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import time
 
 app = Flask(__name__)
-ads = []  # временное хранилище
+CORS(app)
+
+ads = []
 
 @app.route("/")
 def home():
